9acdf4f6113a9cbffc6b4ae517bf420906f701ec
Sanjith Venkatesh <sanjith@pacbell.net>
2020-05-02 16:44:48 -0700

    Order confirmation page

--- orderConfirmation.php
+++ orderConfirmation.php
@@ -3,13 +3,14 @@
     <div class="container">
         <?=file_get_contents("components/header.html");?>
         <div class="main">
-            <div class="content" id="mainContent" style="padding-bottom: 10px;">
-                <h1>Your order has been confirmed.</h1>
-                <a href="../index.html" style="margin-left: 20px;">If you don't click here in the next five seconds, you will be automatically redirected.</a>
-                <br>
+            <div class="content">
+                <h1>Thank you for your order <?php echo $_POST["firstname"]." ".$_POST["lastname"]?></h1>
+                <p>Email : <?php echo $_POST["email"];?><br>
+                <p>Product ID: <?php echo $_POST["productid"];?></p>
+                <p>Quantity: <?php echo $_POST["quantity"];?></p>
             </div>
-        <?=file_get_contents("components/footer.html");?>
+        <?=file_get_contents("components/header.html");?>
     </div>
-    <script type="text/javascript" src="js/orderConfirm.js"></script>
+    <script type="text/javascript" src="../main.js"></script>
 </body>
 </html>
