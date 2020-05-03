8a1999a8b80aefd251b51c1fe375eb588657c776
Sanjith <sanjith@pacbell.net>
2020-05-03 16:11:15 -0700

    got order confirmation good

--- php/createTable.php
+++ php/createTable.php
@@ -1,7 +1,7 @@
 <?php
     require_once "dbconnect.php";
     $tables['orders'] = "CREATE TABLE orders (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        id INT(6) UNSIGNED PRIMARY KEY,
         firstname VARCHAR(30) NOT NULL,
         lastname VARCHAR(30) NOT NULL,
         email VARCHAR(30) NOT NULL,
