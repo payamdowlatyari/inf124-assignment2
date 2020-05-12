6053e22fc0147bc0a82ac9bc6864800b607643fc
Sanjith <sanjith@pacbell.net>
2020-05-11 21:45:09 -0700

    added stuff for order confirmation

--- orderConfirmation.php
+++ orderConfirmation.php
@@ -7,23 +7,55 @@
         $orderId = $_GET["orderid"];
         $sql = "SELECT * FROM orders WHERE id=".$orderId;
         foreach($pdo->query($sql) as $row) {
-            $name = $row['firstname'].$row['lastname'];
+            $name = $row['firstname']." ".$row['lastname'];
             $email = $row['email'];
             $pid = $row['productid'];
             $quantity = $row['quantity'];
+            $email = $row['email'];
+            $phone = $row['phone'];
+            $address = $row['address'];
+        }
+        $productSql = "SELECT * FROM products WHERE id=".$pid;
+        foreach($pdo->query($productSql) as $pRow){
+            $productName = $pRow['name'];
+            $productCategory = $pRow['category'];
+            $productPrice = $pRow['price'];
+            $productPic = $pRow['thumbnail'];
         }
     ?>
     <div class="container">
         <?=file_get_contents("components/header.html");?>
         <div class="main">
-            <div class="content">
+            <div class="content" style="margin-left: 180px;">
                 <h1>Thank you for your order <?php echo $name;?></h1>
-                <p>Email : <?php echo $email;?><br>
-                <p>Product ID: <?php echo $pid;?></p>
-                <p>Quantity: <?php echo $quantity;?></p>
-                <br/><br/>
+                <div style="display: flex;">
+                    <div class="orderConfirmation">
+                        <h2 style="margin-left: 20px;">Order Information</h2><br/>
+                        <p style="margin-left: 40px;">Order ID: <?php echo $orderId;?></p>
+                        <p style="margin-left: 40px;">Email : <?php echo $email;?></p>
+                        <p style="margin-left: 40px;">Phone Number : <?php echo $phone;?></p>
+                        <br/>
+                        <h2 style="margin-left: 20px;">Product Information</h2><br/>
+                        <p style="margin-left: 40px;">Product: <?php echo $productName;?></p>
+                        <p style="margin-left: 40px;">Quantity: <?php echo $quantity;?></p>
+                        <p style="margin-left: 40px;">Price per Product: <?php echo $productPrice;?></p>
+                        <br/>
+                        <h2 style="margin-left: 20px;">Shipping Information</h2><br/>
+                        <p style="margin-left: 40px;">Method: <?php echo $row['method'];?></p>
+                        <p style="margin-left: 40px;">Address: <?php echo $row['address'];?></p>
+                        <p style="margin-left: 40px;">City: <?php echo $row['city'];?></p>
+                        <p style="margin-left: 40px;">State: <?php echo $row['state'];?></p>
+                        <p style="margin-left: 40px;">Zip Code: <?php echo $row['zip'];?></p>
+                    </div>
+                    <div>
+                        <img src="assets/<?= $productPic; ?>" class="thumbnail" width="175"
+                                alt="<?= $productName; ?>" style="margin-left:100px;margin-top: 100px;"/>
+                    </div>
+                </div> 
             </div>
+            <br/>
         <?=file_get_contents("components/footer.html");?>
     </div>
+
 </body>
 </html>
